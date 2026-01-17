a9def18270d592de60d7106f176d0306dd0b73f1
Cornel-mihai Badea <cornel@cornel-mihai.alienware.local>
2026-01-17 07:48:08 +0100

    moved
    Committer: Cornel-mihai Badea <cornel@cornel-mihai.alienware.local>

--- ssha_gen.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-PASSWORD="$1"
-SALT="$(openssl rand 4)"
-SHA1="$(printf "%s%s" "$PASSWORD" "$SALT" | openssl dgst -binary -sha1)"
-printf "{SSHA}%s\n" "$(printf "%s%s" "$SHA1" "$SALT" | base64)"
